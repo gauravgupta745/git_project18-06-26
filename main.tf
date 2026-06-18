804185a2d9843c67aee54abf797afcdab8a930a8
gauravgupta745 <gaurav.guptagn4@gmail.com>
2026-06-18 14:03:15 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example"
+resource "azurerm_resource_group" "rg" {
+  name     = "rgd11"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "rg" {
+  name     = "rgd33"
+  location = "West Europe"
+}
+
